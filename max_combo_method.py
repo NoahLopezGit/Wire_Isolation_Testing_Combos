76648a845d40223cb0473e4f7358914bdf7641c1
Noah Lopez <loalno12@gmail.com>
2024-03-29 23:53:15 -0700

    fixing pin count issue again

--- max_combo_method.py
+++ max_combo_method.py
@@ -12,7 +12,7 @@ def remove_combos(tested_combos, combos):
     return [combo for combo in combos if combo not in tested_combos]
 
 # generate all pairs for a given pin count
-pin_count=100
+pin_count=50
 combos =  list(itertools.combinations(range(1,pin_count+1),2))
 
 #iterate over each pin, add pin if it increases the tested combos
@@ -21,7 +21,7 @@ sets = []
 while len(combos)>0:
     pins = []
     previous_amount_tested = 0
-    for pin in range(1,pin_count):
+    for pin in range(1,pin_count+1):
         if len(pins)>pin_count//2:
             break
         new_amount_tested = len(tested_combos(pins + [pin], combos))
